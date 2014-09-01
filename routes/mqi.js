9a9a51792efe714cf8343a43ec23d1fb399bafcc
Mahesh Chari <maheshchari@gmail.com>
2014-09-01 17:18:40 +0530

    PB raster api proxied

--- routes/mqi.js
+++ routes/mqi.js
@@ -25,11 +25,9 @@ module.exports = function (app){
     app.all('/api/organisation',User.authorize,proxy_route('http://mqciscocls.mqidentity.net:8080/fid-OrganizationAddOn'));
     app.all('/api/subscriber',User.authorize,proxy_route('http://mqciscocls.mqidentity.net:8080/fid-SubscriberAddOn'));
     app.all('/api/parking',User.authorize,proxy_route('http://mqciscocls.mqidentity.net:8080/fid-SmartParkingGateway'));
-  //  app.all('/api/subscriber',User.authorize,proxy_route('http://mqciscocls.mqidentity.net:8080/fid-SubscriberAddOn'));
-
-    app.get('/api/poi',function (req,res){
+    app.get('/api/poi',User.authorize,function (req,res){
         console.log(req.query);
-        proxy = request.get({
+        var proxy = request.get({
             uri:'http://192.168.100.244:8080/rest/poiservice/results.json',
             qs:req.query,
             headers: {
@@ -42,6 +40,31 @@ module.exports = function (app){
                console.log('got response')
             }else
             console.log(response)
+        });
+        req.pipe(proxy).pipe(res);
+    });
+    app.post('/api/raster/:width/:height/:bbox',function (req,res){
+
+        var width= req.param('width');
+        var height= req.param('height');
+        var bbox= req.param('bbox');
+
+        if(!width || !height || !bbox )return res.send(500,'Invalid Parameters')
+
+        var proxy = request.post({
+            uri:'http://192.168.100.244:8080/rest/Spatial/MappingService/maps/image.png;w='+width+';h='+height+';b='+bbox,
+            qs:req.query,
+            json: req.body,
+            headers: {
+                'User-Agent': 'request',
+                'Authorization':'Basic YWRtaW46YWRtaW4=',
+                'Username':'admin',
+                'Password':'admin'
+            }},function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log('got response')
+            }else
+                console.log(response)
         });
         req.pipe(proxy).pipe(res);
     });
