5f04e239e9ed3c1c5e8eeb3790ab73c3c26dba4b
Mahesh Chari <maheshchari@gmail.com>
2014-09-22 13:58:27 +0530

    add offer to july system when a offer is added on portal server Update offer to  july system when a offer is updated on portal server delete offer from  july system when a offer is delete  on portal server add event to july system when a event is added on portal server Update event to  july system when a event is updated on portal server delete event from  july system when a event is deleted  on portal server

--- routes/offers.js
+++ routes/offers.js
@@ -4,12 +4,36 @@ var restifyMongoose = require('restify-mongoose');
 var Offer = mongoose.model('offer');
 var offers = restifyMongoose(Offer);
 var User=mongoose.model('User');
+var request=require('request');
+var TIMEOUT=60000;
+var url="http://54.81.94.142:8080/notification/Offer/chicago/";
+
 module.exports = function (app){
-    app.get('/offers', offers.query());
+
+    function offer_post(model){
+        request.post({uri: url, json: model,timeout:TIMEOUT},function(error, response, body){
+            if(error)return console.log('posting offer to july system failed.'+error);
+            console.log('offer posted to july system.');
+            console.log(body);
+        });
+    };
+
+    function offer_delete(model){
+        request.del({uri: url+model._id, json: model,timeout:TIMEOUT},function(error, response, body){
+            if(error)return console.log('deleting  offer from july system failed.'+error)
+            console.log('offer delete from july system.');
+            console.log(response);
+        });
+    };
+
+    app.get('/offers',offers.query());
     app.get('/offers/:id', offers.detail());
     app.post('/offers', offers.insert());
     app.post('/offers/:id', offers.update());
     app.del('/offers/:id', offers.remove());
+    offers.on('update',offer_post);
+    offers.on('insert',offer_post );
+    offers.on('remove',offer_delete);
 }
 
 
