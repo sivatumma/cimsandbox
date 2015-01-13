dd1f6e94f2c895f91de8ba6f4a2de9f7ccc92585
root <root@ip-172-19-0-25.ap-southeast-1.compute.internal>
2015-01-13 15:43:59 +0000

    Committing uncommitting changes on server

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ var path = require('path')
 
 module.exports = function (env){
     var main={
-      database:'mongodb://10.10.0.27/cls',
+      database:'mongodb://172.19.1.11/cls',
       app_name:'CLS mobile app',
       app_root:root_path,
       temp:path.join('D:','temp'),
       certificates_dir:path.join('C:','self-signed'),
-      mobile_app_root:path.join('D:','CLS-Mobile-App'),
-      portal_app_root:path.join('D:','CLS-Portal-App'),
-      mobile_app_debug_root:path.join('D:','CLS-Mobile-App-Debug')
+      mobile_app_root:path.join('D:','cisco-sandbox-mobile-app'),
+      portal_app_root:path.join('D:','cisco-sandbox-portal-app'),
+      mobile_app_debug_root:path.join('D:','cisco-sandbox-mobile-app-Debug')
     };
 
     var dev={
@@ -22,9 +22,9 @@ module.exports = function (env){
         temp:path.join('/','tmp'),
         env:'production',
         certificates_dir:path.join('/','etc','ssl','self-signed'),
-        mobile_app_root:path.join('/','opt','CLS-Mobile-App'),
-        portal_app_root:path.join('/','opt','CLS-Portal-App'),
-        mobile_app_debug_root:path.join('/','opt','CLS-Mobile-App-Debug')
+        mobile_app_root:path.join('/','opt','cisco-sandbox-mobile-app'),
+        portal_app_root:path.join('/','opt','cisco-sandbox-portal-app'),
+        mobile_app_debug_root:path.join('/','opt','cisco-sandbox-mobile-app-Debug')
     }
 
     return _.extend(main,(env=='dev')?dev:prod);
